4628e6e5c7196d56d494cae4c42e5a17f671fdb0
Yohei Kinowaki <kinowaki.y@gmail.com>
2025-02-24 16:41:08 +0900

    Improve test with strange input

--- test.bash
+++ test.bash
@@ -11,7 +11,12 @@ res=0
 
 ### I/O test ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng $LINE0
+[ "${out}" = 15 ] || ng $LINENO
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
