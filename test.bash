c144e462fc9435644a6c6ee6dbc9993f3d71b8a4
Yohei Kinowaki <kinowaki.y@gmail.com>
2025-02-24 16:27:58 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Yohei Kinowaki
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng(){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O test ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng $LINE0
 
-[ "${out}" = 15 ]
+[ "$res" = 0 ] && echo OK
+exit $res
